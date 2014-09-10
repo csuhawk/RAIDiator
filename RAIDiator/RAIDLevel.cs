af562531268fa6138a01a7f5c1c2ee18ae70a5f5
Joel Ambass <ambass.joel@gmail.com>
2014-09-10 21:48:45 +0200

    Added Basic RAID-Level

---
+++ RAIDiator/RAIDLevel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAIDiator
+{
+    /// <summary>
+    /// Represents a RAIDLevel
+    /// </summary>
+    abstract class RAIDLevel
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public RAIDLevel()
+        {
+
+        }
+
+        /// <summary>
+        /// Minimal number of HDDs
+        /// </summary>
+        protected int minHDD;
+
+        /// <summary>
+        /// Name of the RAID-Level
+        /// </summary>
+        protected String name;
+
+        /// <summary>
+        /// Name of the RAID-Level
+        /// </summary>
+        public String Name
+        {
+            get { return name; }
+        }
+
+        /// <summary>
+        /// Minimal number of HDDs
+        /// </summary>
+        public int MinHDD 
+        {
+            get { return minHDD; }
+        }
+
+        /// <summary>
+        /// Calculates how many HDD-Space is usable
+        /// </summary>
+        /// <returns></returns>
+        abstract public double calcUsableDiskSpace();
+    }
+}
